bcd3bd0c5ca9f1738c70823d03541d9c2dbaf638
Stormcrowd <165189936+Stormcrowd@users.noreply.github.com>
2024-06-11 09:39:22 -0500

    configuration.py

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 URL_SERVICE = "https://cnt-815d900f-7ef1-4634-a919-711ccbea7032.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 PRODUCTS_KITS_PATH = "/api/v1/kits"
-
+#.
 #USERS_TABLE_PATH =
